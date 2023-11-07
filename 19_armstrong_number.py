ed2ebf1eb9e698685324c695dc684bdc3c66dd0a
Basant Chaudhary <bmagar932@gmail.com>
2023-11-07 17:39:22 +0545

    -

--- 19_armstrong_number.py
+++ 19_armstrong_number.py
@@ -1,8 +1,20 @@
 # Program to Check Armstrong Number
 num = int(input("Enter number:"))
-
+sum = 0
 length = len(str(num))
-print("length", length)
+for digit in str(num):
+    power_of_digit = int(digit) ** length
+   
+    sum = sum + power_of_digit
+
+if sum == num:
+    print("The number is armstrong number:", num)
+else:
+    print("The nuumber isn't armstrong number:", num)
+    
+    
+
+ 
 
 
 
---
+++ 26_find_HCF_or_GCD.py
@@ -0,0 +1,5 @@
+# Generating HCF
+
+num1 = int(input("Enter large number:"))
+num2 = int(input("Enter small number:"))
+
