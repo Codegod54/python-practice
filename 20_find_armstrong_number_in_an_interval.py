6f36c6aefa29940d0d6a455968bf18e267ec308a
Basant Chaudhary <bmagar932@gmail.com>
2023-11-08 16:24:17 +0545

    -

--- 20_find_armstrong_number_in_an_interval.py
+++ 20_find_armstrong_number_in_an_interval.py
@@ -1,1 +1,12 @@
 # Program to Find Armstrong Number in an Interval
+num = int(input("Enter number:"))
+for number in range(0, num):
+    sum = 0
+    length = len(str(number))
+    for digit in str(number):
+        power_of_digit = int(digit) ** length
+   
+        sum = sum + power_of_digit
+
+    if sum == number:
+        print("The number is armstrong number:", number)
