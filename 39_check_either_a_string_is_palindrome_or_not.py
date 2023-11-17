17a035560ca86fe8564ced4ba2fdc756f8454950
Basant Chaudhary <bmagar932@gmail.com>
2023-11-17 17:27:18 +0545

    -

---
+++ 39_check_either_a_string_is_palindrome_or_not.py
@@ -0,0 +1,10 @@
+# check either a number is palindrome or not
+
+word = str(input("Enter a string:"))
+
+reverseword = word[::-1]
+
+if reverseword == word:
+    print("The string is palindrome", word)
+else:
+    print("The string is not palindrome", word)
